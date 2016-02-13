87730cfbbcc6d277d93696126a4d25374435e4ab
Michael Hoffman <mhoffman999@gmail.com>
2016-02-12 21:11:28 -0500

    First commit

---
+++ BuildUpdaterTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuildUpdaterTest
+//
+//  Created by Michael Hoffman on 2/12/16.
+//  Copyright © 2016 Here We Go. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuildUpdaterTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  BuildUpdaterTest
+//
+//  Created by Michael Hoffman on 2/12/16.
+//  Copyright © 2016 Here We Go. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"From the beginning!");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
